89c38ceb246d4f73d3a4a1b0cd6fd4097542650a
wuwenchao <susices@hotmail.com>
2023-01-30 12:22:50 +0800

    update texture

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -9,6 +9,7 @@
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
 #include "Shader.h"
+#include "Texture.h"
 #include "VertexArray.h"
 #include "VertexBufferLayout.h"
 
@@ -48,21 +49,24 @@ int main(void)
     {
         float positions[] =
             {
-            -0.5f,-0.5f,
-            0.5f,-0.5f,
-            0.5f,0.5f,
-            -0.5f,0.5f,
+            -0.5f,-0.5f,0.0f,0.0f,//0
+            0.5f,-0.5f,1.0f,0.0f,//1
+            0.5f,0.5f,1.0f,1.0f,//2
+            -0.5f,0.5f,0.0f,1.0f,//3
             };
 
         unsigned int indices[] = {
             0,1,2,
             2,3,0,
         };
-
+        GLCall(glEnable(GL_BLEND))
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA))
+        
 
         VertexArray vertex_array;
-        VertexBuffer vertex_buffer(positions, 4*2*sizeof(float));
+        VertexBuffer vertex_buffer(positions, 4*4*sizeof(float));
         VertexBufferLayout vertex_buffer_layout;
+        vertex_buffer_layout.Push<float>(2);
         vertex_buffer_layout.Push<float>(2);
         vertex_array.AddBuffer(vertex_buffer, vertex_buffer_layout);
 
@@ -71,6 +75,10 @@ int main(void)
         Shader shader("res/shaders/Basic.shaderfile");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f,0.3f,0.8f,1.0f);
+
+        Texture texture("res/textures/image.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture",0);
 
         // clear bind data
         vertex_array.Unbind();
--- OpenGL/src/Texture.cpp
+++ OpenGL/src/Texture.cpp
@@ -10,20 +10,30 @@ Texture::Texture(const std::string& path):m_RendererID(0),m_FilePath(path),m_Loc
 
     GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR))
     GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR))
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE))
+    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE))
 
+    GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_LocalBuffer))
+    GLCall(glBindTexture(GL_TEXTURE_2D,0))
+
+    if (m_LocalBuffer)
+    {
+        stbi_image_free(m_LocalBuffer);
+    }
 }
 
 Texture::~Texture()
 {
-    
+    GLCall(glDeleteTextures(1,&m_RendererID))
 }
 
-void Texture::Bind(unsigned slot) const
+void Texture::Bind(unsigned int slot) const
 {
-    
+    GLCall(glActiveTexture(GL_TEXTURE0 + slot))
+    GLCall(glBindTexture(GL_TEXTURE_2D, m_RendererID))
 }
 
 void Texture::Unbind()
 {
-    
+    GLCall(glBindTexture(GL_TEXTURE_2D,0))
 }
